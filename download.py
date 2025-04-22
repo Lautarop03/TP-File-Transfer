295492bac7e2e4f0852301263eb1eaa72d2c863b
DanielMarianetti <74010816+DanielMarianetti@users.noreply.github.com>
2025-04-22 12:38:59 -0300

    Add initial args management and project folder structure scaffold

---
+++ download.py
@@ -0,0 +1,16 @@
+import argparse
+from lib.client import download
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='download',
+        usage='download [-h] [-v | -q] [-H ADDR] [-p PORT] [-d FILEPATH] [-n FILENAME] [-r protocol]',
+        description='Download a file placed in FILEPATH on the server running in ADDR:PORT and save it as FILENAME',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    download.add_arguments(parser)
+    args = parser.parse_args()
+    download.run(args)
+
+if __name__ == '__main__':
+    main()
---
+++ lib/server/start_server.py
@@ -0,0 +1,20 @@
+def add_arguments(parser):
+    verbosity = parser.add_mutually_exclusive_group()
+    verbosity.add_argument('-v', '--verbose', action='store_true', default=False, help='increase output verbosity')
+    verbosity.add_argument('-q', '--quiet', action='store_true', default=True, help='decrease output verbosity')
+
+    parser.add_argument('-H', '--host', type=str, default='0.0.0.0', metavar='', help='service IP address')
+    parser.add_argument('-p', '--port', type=int, default=5000, metavar='', help='service port')
+    parser.add_argument('-s', '--storage', type=str, default='/', required=False, metavar='', help='storage dir path')
+    parser.add_argument('-r', '--protocol', type=str, choices=['sw', 'sr'], default='sw', metavar='', help='error recovery protocol')
+
+def run(args):
+
+    if(args.verbose):
+        print("=== Server Config ===")
+        print(f"Verbose      : {args.verbose}")
+        print(f"Quiet        : {args.quiet}")
+        print(f"Host         : {args.host}")
+        print(f"Port         : {args.port}")
+        print(f"Storage Path : {args.storage}")
+        print(f"Protocol     : {args.protocol}")
